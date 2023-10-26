15e99c0517a604fa975cb14d771bb748a341174d
Gilad Wagner <gilad@cornix.io>
2023-10-26 17:56:46 +0300

    .

--- python/ccxt/bybit.py
+++ python/ccxt/bybit.py
@@ -6200,9 +6200,6 @@ class bybit(Exchange):
                 else:
                     signature = self.hmac(self.encode(authFull), self.encode(self.secret), hashlib.sha256)
                 headers['X-BAPI-SIGN'] = signature
-                if method == 'POST':
-                    if self.partner_name:
-                        headers['Referer'] = self.partner_name
             else:
                 query = self.extend(params, {
                     'api_key': self.apiKey,
@@ -6231,11 +6228,12 @@ class bybit(Exchange):
                         headers = {
                             'Content-Type': 'application/json',
                         }
-                    if self.partner_name:
-                        headers['Referer'] = self.partner_name
                 else:
                     url += '?' + self.rawencode(sortedQuery)
                     url += '&sign=' + signature
+            if method == 'POST':
+                if self.partner_name:
+                    headers['Referer'] = self.partner_name
         return {'url': url, 'method': method, 'body': body, 'headers': headers}
 
     def handle_errors(self, httpCode, reason, url, method, headers, body, response, requestHeaders, requestBody):
