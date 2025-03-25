3fe25c8b3d115ef3dc74231f9559e1fc4707f949
Ori Kalinski <ori@cornix.io>
2025-03-25 05:32:47 +0200

    fix

--- python/ccxt/hyperliquid.py
+++ python/ccxt/hyperliquid.py
@@ -548,7 +548,7 @@ class hyperliquid(Exchange, ImplicitAPI):
         maker = self.safe_number(fees, 'maker')
         amountPrecision = self.safe_integer(market, 'szDecimals')
         return self.safe_market_structure({
-            'id': symbol,
+            'id': baseId,
             'symbol': symbol,
             'base': base,
             'quote': quote,
