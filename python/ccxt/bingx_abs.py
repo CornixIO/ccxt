9cd97cb27b9d8850c3660bca5040466138b2b94f
Adam Zidiker <adam@cornix.io>
2025-04-09 11:52:20 +0300

    add cornix broker

--- python/ccxt/bingx_abs.py
+++ python/ccxt/bingx_abs.py
@@ -2,6 +2,10 @@ from ccxt.bingx import bingx
 
 
 class bingx_abs(bingx):
+    def __init__(self, config={}):
+        super().__init__(config)
+        self.options['broker'] = 'Cornix'
+
     def is_inverse(self):
         default_type = self.safe_string(self.options, 'defaultType')
         return default_type == 'inverse'
