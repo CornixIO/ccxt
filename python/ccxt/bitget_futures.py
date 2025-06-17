eb4515ed919ac1daf5e7aefe802e957bb9ae78e9
Adam Zidiker <adam@cornix.io>
2025-06-17 16:43:57 +0300

    fix bitget locale

--- python/ccxt/bitget_futures.py
+++ python/ccxt/bitget_futures.py
@@ -1,9 +1,9 @@
-from ccxt.bitget import bitget
+from ccxt.bitget_abs import bitget_abs
 
 BITGET_FUTURES = 'Bitget Futures'
 
 
-class bitget_futures(bitget):
+class bitget_futures(bitget_abs):
     def __init__(self, config={}):
         super().__init__(config)
         self.options['defaultType'] = 'swap'
--- python/ccxt/bitget_inverse.py
+++ python/ccxt/bitget_inverse.py
@@ -1,9 +1,9 @@
-from ccxt.bitget import bitget
+from ccxt.bitget_abs import bitget_abs
 
 BITGET_INVERSE = 'Bitget Inverse'
 
 
-class bitget_inverse(bitget):
+class bitget_inverse(bitget_abs):
     def __init__(self, config={}):
         super().__init__(config)
         self.options['defaultType'] = 'swap'
