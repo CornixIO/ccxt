af4e413443895bd3043febfec2aba61483ea23f5
Ori Kalinski <orikalinski@gmail.com>
2025-06-24 17:25:47 +0300

    adding reject reason for hyperliquid

--- python/ccxt/hyperliquid_abs.py
+++ python/ccxt/hyperliquid_abs.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from ccxt.base.errors import ExchangeNotAvailable
 from ccxt.base.errors import PermissionDenied
-from ccxt.base.types import Str, Int
+from ccxt.base.types import Str, Int, Market, Order
 from ccxt.hyperliquid import hyperliquid
 
 
@@ -24,6 +24,13 @@ class hyperliquid_abs(hyperliquid):
         if parsed_status := statuses.get(status):
             return parsed_status
         return super().parse_order_status(status)
+
+    def parse_order(self, order: dict, market: Market = None) -> Order:
+        order_dict = super().parse_order(order, market=market)
+        exchange_status = self.safe_string_2(order, 'status', 'ccxtStatus')
+        if exchange_status == 'positionIncreaseAtOpenInterestCapRejected':
+            order_dict['reject_reason'] = 'exceeds maximum limit allowed'
+        return order_dict
 
     @staticmethod
     def replace_symbol_k_with_1000(symbol: Str):
