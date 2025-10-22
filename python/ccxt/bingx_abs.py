b98ec8c343c74d05ae467069cc42df5fbd8a6f14
Ori Kalinski <orikalinski@gmail.com>
2025-10-22 11:45:44 +0300

    fix

--- python/ccxt/bingx_abs.py
+++ python/ccxt/bingx_abs.py
@@ -1,5 +1,6 @@
 from typing import Any
-from ccxt.base.errors import PermissionDenied, OrderNotFound, OperationFailed, InsufficientFunds, BadRequest
+from ccxt.base.errors import PermissionDenied, OrderNotFound, OperationFailed, InsufficientFunds, BadRequest, \
+    ExchangeError
 from ccxt.bingx import bingx
 
 
@@ -15,6 +16,7 @@ class bingx_abs(bingx):
             },
             'exceptions': {
                 'exact': {
+                    '80012': ExchangeError,
                     '100413': PermissionDenied,
                     '101253': InsufficientFunds,  # {"code":101253,"msg":"Insufficient margin","data":{}}
                     '109400': BadRequest,
