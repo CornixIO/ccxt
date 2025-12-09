7388be1bfe4be5f88241c967151ad8e26b60151b
Adam Zidiker <adam@cornix.io>
2025-12-09 11:47:44 +0200

    change binance futures to use algo order

--- python/ccxt/binance_futures.py
+++ python/ccxt/binance_futures.py
@@ -1,8 +1,8 @@
 from typing import Any
 
 from ccxt.binance_futures_abs import binance_futures_abs
-from ccxt.base.errors import ExchangeError
-from ccxt.base.types import Market
+from ccxt.base.errors import ExchangeError, OrderNotFound
+from ccxt.base.types import Str
 
 BINANCE_FUTURES = 'Binance Futures'
 
@@ -26,3 +26,28 @@ class binance_futures(binance_futures_abs):
                 },
             }
         })
+
+    def parse_order_status(self, status: Str):
+        if status == 'TRIGGERED':
+            return 'open'
+        return super().parse_order_status(status)
+
+    def fetch_order(self, id: str, symbol: Str = None, params={}):
+        if params.get('type') == 'stop':
+            params.pop('type')
+            try:
+                return super().fetch_order(id, symbol, params)
+            except OrderNotFound:
+                return super().fetch_order(id, symbol, params | {'stop': True})
+        else:
+            return super().fetch_order(id, symbol, params)
+
+    def cancel_order(self, id: str, symbol: Str = None, params={}):
+        if params.get('type') == 'stop':
+            params.pop('type')
+            try:
+                return super().cancel_order(id, symbol, params)
+            except OrderNotFound:
+                return super().cancel_order(id, symbol, params | {'stop': True})
+        else:
+            return super().cancel_order(id, symbol, params)
