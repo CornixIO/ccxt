b402dbb5fd6275b27cdaa453eb9fb87f0cf9afa3
Ori Kalinski <orikalinski@gmail.com>
2026-01-14 16:07:50 +0200

    handling 40013 user abnormal as permission denied

--- python/ccxt/bitget_abs.py
+++ python/ccxt/bitget_abs.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from ccxt import PermissionDenied
 from ccxt.bitget import bitget
 
 
@@ -6,3 +9,12 @@ class bitget_abs(bitget):
         super().__init__(config)
         self.headers['locale'] = 'en-US'
         self.has['fetchCurrencies'] = False
+
+    def describe(self) -> Any:
+        return self.deep_extend(super().describe(), {
+            'exceptions': {
+                'exact': {
+                    '40013': PermissionDenied,  # {"code":"40013","msg":"User status is abnormal","requestTime":1768398859928,"data":null}
+                }
+            }
+        })
