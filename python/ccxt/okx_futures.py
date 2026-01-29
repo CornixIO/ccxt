9df3c7965b789058d83dae3470ae9ed73a395d95
Ori Kalinski <orikalinski@gmail.com>
2026-01-29 16:12:33 +0200

    fix

--- python/ccxt/okx_futures.py
+++ python/ccxt/okx_futures.py
@@ -25,3 +25,25 @@ class okx_futures(okx_abs):
             contract_size_string = self.number_to_string(contract_size)
             quantity_abs_string = Precise.string_mul(contracts_abs_string, contract_size_string)
         return self.parse_number(quantity_abs_string)
+
+    def _apply_contract_size_to_order(self, order, contract_size):
+        if order.get('amount') is not None:
+            order['amount'] = self.parse_number(Precise.string_mul(str(order['amount']), str(contract_size)))
+        if order.get('filled') is not None:
+            order['filled'] = self.parse_number(Precise.string_mul(str(order['filled']), str(contract_size)))
+        if order.get('remaining') is not None:
+            order['remaining'] = self.parse_number(Precise.string_mul(str(order['remaining']), str(contract_size)))
+        return order
+
+    def parse_order(self, order: dict, market: Market = None) -> Order:
+        order = super().parse_order(order, market)
+        if market is None and order.get('symbol'):
+            try:
+                market = self.market(order['symbol'])
+            except Exception:
+                market = None
+        if market is not None:
+            contract_size = market.get('contractSize')
+            if contract_size is not None:
+                order = self._apply_contract_size_to_order(order, contract_size)
+        return order
