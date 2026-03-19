2406cfa5190b23f320f00a65414626798c207c21
Ori Kalinski <orikalinski@gmail.com>
2026-03-19 09:32:23 +0200

    fix

--- python/ccxt/coinbase_advanced_spot.py
+++ python/ccxt/coinbase_advanced_spot.py
@@ -8,6 +8,7 @@ COINBASE_ADVANCED_SPOT = 'Coinbase Advanced Spot'
 class coinbase_advanced_spot(coinbase):
     def __init__(self, config={}):
         super().__init__(config)
+        self.options['fetchBalance'] = 'v3PrivateGetBrokerageAccounts'
 
     def fetch_order_trades(self, id: str, symbol: Optional[str] = None, since: Optional[int] = None,
                            limit: Optional[int] = None, params={}):
