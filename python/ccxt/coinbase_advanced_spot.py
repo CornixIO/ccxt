d740b9979e9b9e60e8a55351d30a047dd9d74501
Ori Kalinski <orikalinski@gmail.com>
2026-03-19 09:18:54 +0200

    update coinbase to ccxt master

--- python/ccxt/coinbase_advanced_spot.py
+++ python/ccxt/coinbase_advanced_spot.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from ccxt.coinbase import coinbase
 
 COINBASE_ADVANCED_SPOT = 'Coinbase Advanced Spot'
@@ -6,3 +8,8 @@ COINBASE_ADVANCED_SPOT = 'Coinbase Advanced Spot'
 class coinbase_advanced_spot(coinbase):
     def __init__(self, config={}):
         super().__init__(config)
+
+    def fetch_order_trades(self, id: str, symbol: Optional[str] = None, since: Optional[int] = None,
+                           limit: Optional[int] = None, params={}):
+        request = {'order_id': id}
+        return self.fetch_my_trades(symbol, since, limit, self.extend(request, params))
