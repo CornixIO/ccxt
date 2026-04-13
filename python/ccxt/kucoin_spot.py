9c6e5a582d4cc80da102feb9c5acc6ea3eef9c1e
Ori Kalinski <orikalinski@gmail.com>
2026-04-13 17:24:15 +0300

    fix

--- python/ccxt/kucoin_spot.py
+++ python/ccxt/kucoin_spot.py
@@ -18,13 +18,3 @@ class kucoin_spot(KucoinAbs, kucoin):
         if order and order.get('stop') == '':
             order = {**order, 'stop': None}
         return super().parse_order(order, market)
-
-    def fetch_markets(self, params={}):
-        markets = super().fetch_markets(params)
-        for market in markets:
-            precision = market.get('precision') or {}
-            for key in ('amount', 'price'):
-                val = precision.get(key)
-                if isinstance(val, int) and val > 0:
-                    precision[key] = 10 ** -val
-        return markets
