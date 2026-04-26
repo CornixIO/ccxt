a354978847d4c511ce0af5068a511f63047e6307
Ori Kalinski <orikalinski@gmail.com>
2026-04-26 14:40:20 +0300

    handling DDoSProtection for mexc

--- python/ccxt/mexc_abs.py
+++ python/ccxt/mexc_abs.py
@@ -1,4 +1,5 @@
 from ccxt.mexc import mexc
+from ccxt.base.errors import DDoSProtection
 
 MEXC = 'MEXC'
 
@@ -7,3 +8,4 @@ class mexc_abs(mexc):
     def __init__(self, config={}):
         super().__init__(config)
         self.options['broker'] = 'CORNIX'
+        self.exceptions['exact']['510'] = DDoSProtection  # {"success":false,"code":510,"message":"Requests are too frequent, please try again later"}
