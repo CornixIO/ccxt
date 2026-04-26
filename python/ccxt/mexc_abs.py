315b9a3a1a2f005aa41b29ddc6027d7c3d1def3c
Ori Kalinski <orikalinski@gmail.com>
2026-04-26 14:41:05 +0300

    handling DDoSProtection for mexc

--- python/ccxt/mexc_abs.py
+++ python/ccxt/mexc_abs.py
@@ -5,7 +5,15 @@ MEXC = 'MEXC'
 
 
 class mexc_abs(mexc):
+    def describe(self):
+        return self.deep_extend(super(mexc_abs, self).describe(), {
+            'exceptions': {
+                'exact': {
+                    '510': DDoSProtection,  # {"success":false,"code":510,"message":"Requests are too frequent, please try again later"}
+                },
+            },
+        })
+
     def __init__(self, config={}):
         super().__init__(config)
         self.options['broker'] = 'CORNIX'
-        self.exceptions['exact']['510'] = DDoSProtection  # {"success":false,"code":510,"message":"Requests are too frequent, please try again later"}
